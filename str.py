062c9885876c10fcdf1e40d77dc25d21f3a41213
Montaha Hossain <127191848+Montaha3d@users.noreply.github.com>
2025-08-02 11:45:31 +0600

    Create str.py

---
+++ str.py
@@ -0,0 +1,18 @@
+computer_parts_list = ["Ram", "Motherboard", "Powersupply", "Processor"]
+for items in computer_parts_list:
+     print(items)
+
+ name = "Montaha"
+for single_word in name:
+    print(single_word)
+
+for count in range(10):
+    print(count)
+
+quantity = {"Banana" : 4, "Shak" : 2, "Oil" : 3, "Fish": 2}
+for objects,quantitys in quantity.items():
+    # print(quantitys)
+    print("Khabar: " + objects, "Quantity: " + str(quantitys))
+
+
+
